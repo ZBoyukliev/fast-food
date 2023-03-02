1f18caa2ef0645f85bfee0efdb12e357df27fef1
ZBoyukliev <zarkoboyukliev@gmail.com>
2023-03-02 07:59:58 +0000

    Add css for trolley

--- src/components/Menu/Trolley/Trolley.js
+++ src/components/Menu/Trolley/Trolley.js
@@ -2,14 +2,26 @@ import styles from './Trolley.module.css';
 
 const Trolley = () => {
     return (
-        <div className={styles[`trolley`]}>
-        <header className={styles[`trolley-head`]}>
-            КОЛИЧКА
-        </header>
-        <p>
-            В момента нямате добавени артикули в количката
-        </p>
-    </div>
+        <div className={styles[`outer`]}>
+            <div className={styles[`trolley`]}>
+
+                <header className={styles[`trolley-head`]}>
+                    <h3 className={styles[`trolley-head-tittle`]}><i class="fa-solid fa-cart-shopping"></i>КОЛИЧКА</h3>
+                </header>
+
+                <div className={styles[`items`]}>
+                    <ul className={styles[`items-aded`]}>
+                        <div className={styles[`items-aded-inner`]}>
+                            <li className={styles[`items-aded-li`]}>
+                                В момента нямате добавени артикули в количката
+                            </li>
+                        </div>
+                    </ul>
+                </div>
+
+
+            </div>
+        </div>
     );
 };
 
