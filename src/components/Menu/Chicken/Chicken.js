9f696adf77965abfb63b029289576c5fa2e5a4b2
ZBoyukliev <zarkoboyukliev@gmail.com>
2023-03-10 19:20:19 +0200

    Add items in server

--- src/components/Menu/Chicken/Chicken.js
+++ src/components/Menu/Chicken/Chicken.js
@@ -1,8 +1,18 @@
 import Subnav from '../Subnav/Subnav';
 import Trolley from '../Trolley/Trolley';
 import styles from './Chicken.module.css';
+import { useEffect, useState } from 'react';
+import * as menuService from '../../../services/menuService';
 
 const Chicken = () => {
+
+    const [chicken, setChicken] = useState([]);
+
+    useEffect(() => {
+        menuService.getByCategory('chicken')
+            .then(res => setChicken(res));
+    }, []);
+
     return (
         <>
             <Subnav />
@@ -16,7 +26,7 @@ const Chicken = () => {
                         <h3>ПИЛЕ</h3>
                     </div>
                     <div className={styles['menu-sec']}>
-                        <div className={styles['menu-sec-product']}>
+                        {/* <div className={styles['menu-sec-product']}>
                             <h3 className={styles['menu-sec-title']}>ТЕЛЕШКО ИЗКУШЕНИЕ</h3>
                             <img src='./images/TM_580x500.png' alt='meal' />
                             <div className={styles['menu-price']}>
@@ -29,82 +39,25 @@ const Chicken = () => {
                                 <i class="fa-solid fa-circle-info" />
                                 ИЗБЕРИ МЕНЮ
                             </button>
-                        </div>
+                        </div> */}
 
-                        <div className={styles['menu-sec-product']}>
-                            <h3 className={styles['menu-sec-title']}>ROLLER</h3>
-                            <img src='./images/roller.png' alt='meal' />
-                            <div className={styles['menu-price']}>
-                                <span>5.</span>
-                                <span>50</span>
-                                <span>лв.</span>
-                            </div>
-                            <div className={styles['div-btn']}>
-                                <button className={styles['btn']}>МЕНЮ</button>
-                                <button className={styles['btn']}>ДЕТАЍЛИ</button>
-                                <button className={styles['btn']}>ДОБАВИ</button>
-                            </div>
-                        </div>
+                        {chicken.map( c => (
+                              <div className={styles['menu-sec-product']}>
+                              <h3 className={styles['menu-sec-title']}>{c.title}</h3>
+                              <img src={c.imageUrl} alt='meal' />
+                              <div className={styles['menu-price']}>
+                                  <span>{c.priceLv}</span>
+                                  <span>{c.priceSt}</span>
+                                  <span>лв.</span>
+                              </div>
+                              <div className={styles['div-btn']}>
+                                  <button className={styles['btn']}>МЕНЮ</button>
+                                  <button className={styles['btn']}>ДЕТАЍЛИ</button>
+                                  <button className={styles['btn']}>ДОБАВИ</button>
+                              </div>
+                          </div>
+                        ))}
 
-                        <div className={styles['menu-sec-product']}>
-                            <h3 className={styles['menu-sec-title']}>ТЕЛЕШКО ИЗКУШЕНИЕ</h3>
-                            <img src='./images/2_Composite_za_obrabotka.png' alt='meal' />
-                            <div className={styles['menu-price']}>
-                                <span>5.</span>
-                                <span>50</span>
-                                <span>лв.</span>
-                            </div>
-                            <div className={styles['div-btn']}>
-                                <button className={styles['btn']}>МЕНЮ</button>
-                                <button className={styles['btn']}>ДЕТАЍЛИ</button>
-                                <button className={styles['btn']}>ДОБАВИ</button>
-                            </div>
-                        </div>
-
-                        <div className={styles['menu-sec-product']}>
-                            <h3 className={styles['menu-sec-title']}>ТЕЛЕШКО ИЗКУШЕНИЕ</h3>
-                            <img src='./images/2_Composite_za_obrabotka.png' alt='meal' />
-                            <div className={styles['menu-price']}>
-                                <span>5.</span>
-                                <span>50</span>
-                                <span>лв.</span>
-                            </div>
-                            <div className={styles['div-btn']}>
-                                <button className={styles['btn']}>МЕНЮ</button>
-                                <button className={styles['btn']}>ДЕТАЍЛИ</button>
-                                <button className={styles['btn']}>ДОБАВИ</button>
-                            </div>
-                        </div>
-
-                        <div className={styles['menu-sec-product']}>
-                            <h3 className={styles['menu-sec-title']}>ТЕЛЕШКО ИЗКУШЕНИЕ</h3>
-                            <img src='./images/2_Composite_za_obrabotka.png' alt='meal' />
-                            <div className={styles['menu-price']}>
-                                <span>5.</span>
-                                <span>50</span>
-                                <span>лв.</span>
-                            </div>
-                            <div className={styles['div-btn']}>
-                                <button className={styles['btn']}>МЕНЮ</button>
-                                <button className={styles['btn']}>ДЕТАЍЛИ</button>
-                                <button className={styles['btn']}>ДОБАВИ</button>
-                            </div>
-                        </div>
-
-                        <div className={styles['menu-sec-product']}>
-                            <h3 className={styles['menu-sec-title']}>ТЕЛЕШКО ИЗКУШЕНИЕ</h3>
-                            <img src='./images/2_Composite_za_obrabotka.png' alt='meal' />
-                            <div className={styles['menu-price']}>
-                                <span>5.</span>
-                                <span>50</span>
-                                <span>лв.</span>
-                            </div>
-                            <div className={styles['div-btn']}>
-                                <button className={styles['btn']}>МЕНЮ</button>
-                                <button className={styles['btn']}>ДЕТАЍЛИ</button>
-                                <button className={styles['btn']}>ДОБАВИ</button>
-                            </div>
-                        </div>
                     </div>
                 </section>
             </main>
