f6e1222489a33b110b3bd7ec162300fa5f0ee551
ZBoyukliev <zarkoboyukliev@gmail.com>
2023-03-30 11:57:41 +0300

    add css to order page

--- src/components/CreateOrder/CreateOrder.js
+++ src/components/CreateOrder/CreateOrder.js
@@ -14,22 +14,26 @@ const CreateOrder = () => {
                         <h3>КОЛИЧКА</h3>
                     </div>
 
-                    <div className={styles['cart-table']}>
+                    <section className={styles['cart-table']}>
                         <div className={styles['cart-row']}>
                             <h3 className={styles['cart-row-title']}>ROLLER</h3>
                             <div className={styles['cart-row-counter']}>
 
-                                <i className="fa-solid fa-minus"></i>
+                                <button className={styles['add-remove-btn']}> - </button>
                                 <input className={styles['cart-input']} defaultValue={1} />
-                                <i className="fa-solid fa-plus"></i>
+                                <button className={styles['add-remove-btn']}> + </button>
                             </div>
 
                             <h5 className={styles['cart-row-price']}>
                                 <span className={styles['cart-row-span']}>8.50</span>лв.
                             </h5>
-                            <button className={styles['del-btn']}>ИЗТРИЙ&#10008;</button>
+                            <button className={styles['del-btn']}>ИЗТРИЙ <span className={styles['x-span']}>&#10008;</span></button>
                         </div>
-                    </div>
+                    </section>
+
+                    <section>
+
+                    </section>
 
                 </section>
             </main>
