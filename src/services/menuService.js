610a51f268013c470de356afe6cfc53a1e2ea05f
Zarko Boyukliev <122691228+ZBoyukliev@users.noreply.github.com>
2023-12-26 13:44:19 +0200

    Update menuService.js

--- src/services/menuService.js
+++ src/services/menuService.js
@@ -20,5 +20,3 @@ export const remove = (foodId) => request.del(`${baseUrl}/menu/${foodId}`);
 export const pagination = (skip) => request.get(`${baseUrl}/menu?offset=${skip}&pageSize=8`);
 
 // export const pagination = (skip, foodCategory) => request.get(`${baseUrl}/menu?where=category%3D%22${foodCategory}%22?offset=${skip}&pageSize=6`);
- 
-
