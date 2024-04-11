b3046c282dc4533d55bad8537d02257df070eef0
Zarko Boyukliev <122691228+ZBoyukliev@users.noreply.github.com>
2024-04-11 20:35:46 +0300

    Update LogoutGuard.js

--- src/components/commons/LogoutGuard.js
+++ src/components/commons/LogoutGuard.js
@@ -1,12 +1,11 @@
 import { Navigate, Outlet } from 'react-router-dom';
-
 import { AuthContext } from '../../context/AuthContext';
 import { useContext } from 'react';
-
 
 export const LogoutGuard = ({
     children,
 }) => {
+    
     const { user } = useContext(AuthContext);
     
     if (!user.email) {
