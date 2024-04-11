a46ac59b8365cd283eaa8fa4ba2ce693419e919b
Zarko Boyukliev <122691228+ZBoyukliev@users.noreply.github.com>
2024-04-11 20:35:21 +0300

    Update AdminGuard.js

--- src/components/commons/AdminGuard.js
+++ src/components/commons/AdminGuard.js
@@ -1,11 +1,11 @@
 import { Navigate, Outlet } from 'react-router-dom';
-
 import { AuthContext } from '../../context/AuthContext';
 import { useContext } from 'react';
 
 export const AdminGuard = ({
     children,
 }) => {
+    
     const { user } = useContext(AuthContext);
     
     if (!user.admin) {
