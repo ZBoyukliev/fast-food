635eb964975e801668d18ae85a7ef46a9871b389
Zarko Boyukliev <122691228+ZBoyukliev@users.noreply.github.com>
2024-05-09 19:50:36 +0300

    Update menuService.js

--- src/services/menuService.js
+++ src/services/menuService.js
@@ -2,23 +2,13 @@ import * as request from './requester';
 const baseUrl = 'http://localhost:3030/data';
 
 export const getAll = () => request.get(`${baseUrl}/menu`);
- 
 export const getByCategory = (foodCategory) => request.get(`${baseUrl}/menu?where=category%3D%22${foodCategory}%22`);
-
 export const getOffers = () => request.get(`${baseUrl}/menu?where=type%3D%22offer%22`);
-
 export const searchFood = (query) => request.get(`${baseUrl}/menu?where=title%20LIKE%20%22${query}%22`);
- 
 export const getById = (foodId) => request.get(`${baseUrl}/menu/${foodId}`);
- 
 export const create = (foodData) => request.post(`${baseUrl}/menu`, foodData);
- 
 export const edit = (foodId, foodData) => request.put(`${baseUrl}/menu/${foodId}`, foodData);
-
 export const remove = (foodId) => request.del(`${baseUrl}/menu/${foodId}`);
-
 export const pagination = (skip) => request.get(`${baseUrl}/menu?offset=${skip}&pageSize=8`);
-
-// console.log(Number(true));
 
 // export const pagination = (skip, foodCategory) => request.get(`${baseUrl}/menu?where=category%3D%22${foodCategory}%22?offset=${skip}&pageSize=6`);
