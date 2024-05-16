a1ce585fc43ceb2b9f1f7e23f645324205547367
Zarko Boyukliev <122691228+ZBoyukliev@users.noreply.github.com>
2024-05-16 20:16:36 +0300

    Update Logout.js

--- src/components/Logout/Logout.js
+++ src/components/Logout/Logout.js
@@ -1,13 +1,11 @@
 import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../context/AuthContext';
 import { useContext, useEffect, useState } from 'react';
-
 import * as authService from '../../services/authService';
 import Spinner from '../Spinner/Spinner';
 
-
-
 export const Logout = () => {
+    
     const navigate = useNavigate();
     const { user, userLogout } = useContext(AuthContext);
     const [isLoading, setIsLoading] = useState(false);
