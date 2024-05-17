e94475e5eb4ddfa07acfe3637baa7759abbee555
Zarko Boyukliev <122691228+ZBoyukliev@users.noreply.github.com>
2024-05-17 12:51:47 +0300

    Update SideOrders.js

--- src/components/Menu/SideOrders/SideOrders.js
+++ src/components/Menu/SideOrders/SideOrders.js
@@ -1,8 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
-
 import * as menuService from '../../../services/menuService';
-
 import Subnav from '../../Subnav/Subnav';
 import Trolley from '../../Trolley/Trolley';
 import styles from './SideOrders.module.css';
@@ -14,9 +12,7 @@ const SideOrders = ({ src, type }) => {
 
     const [menu, setMenu] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
-    
     const location = useLocation();
-    
     const category = location.pathname.slice(6);
 
     useEffect(() => {
@@ -36,7 +32,6 @@ const SideOrders = ({ src, type }) => {
         <div className={styles['main-container']}>
             {isLoading ? <Spinner /> :
                 <>
-                
                     <Subnav />
                     {menu.length > 0 ?  
                     <main className={styles['main']}>
@@ -45,36 +40,28 @@ const SideOrders = ({ src, type }) => {
                         <section className={styles['container']}>
                             <img src={src} alt="drinks" />
                         </section>
-
                         <section className={styles['menu']}>
                             <div className={styles['menu-title']}>
                                 <h3>{type}</h3>
                             </div>
-
                             <div className={styles['menu-sec']}>
-
                                 {menu.map(d => <SideOrderItem key={d._id} food={d} />)}
-
                             </div>
                         </section>
-
                     </main>  :
                       <main className={styles['main']}>
                       <Trolley />
                       <section className={styles['container']}>
                           <img src={src} alt="drinks" />
                       </section>
-
                       <section className={styles['menu']}>
                           <div className={styles['menu-title']}>
                               <h3>{type}</h3>
                           </div>
-
                           <div className={styles['menu-sec']}>
                              <h2 className={styles['menu-sec-title']}>В МОМЕНТА НЯМА НАЛИЧНИ ПРОДУКТИ.</h2>
                           </div>
                       </section>
-
                   </main>
                     }
                 </>
