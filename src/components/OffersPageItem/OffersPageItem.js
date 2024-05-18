1be5530e96d480bd4117c3e0e74919714d123504
Zarko Boyukliev <122691228+ZBoyukliev@users.noreply.github.com>
2024-05-18 12:51:32 +0300

    Update OffersPageItem.js

--- src/components/OffersPageItem/OffersPageItem.js
+++ src/components/OffersPageItem/OffersPageItem.js
@@ -1,8 +1,6 @@
 import { useEffect, useState } from 'react';
 import { Link, useNavigate, useParams } from 'react-router-dom';
-
 import * as menuService from '../../services/menuService';
-
 import styles from './OffersPageItem.module.css';
 
 const OffersPageItem = () => {
