5b3f7e5466521b8721552f16e20b9fec88fa9908
Zarko Boyukliev <122691228+ZBoyukliev@users.noreply.github.com>
2024-08-26 15:38:43 +0300

    Update setupTests.js

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,3 @@
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
