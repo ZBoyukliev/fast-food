f40a5c8129492525537248b0269a3dcc7321efe1
Zarko Boyukliev <122691228+ZBoyukliev@users.noreply.github.com>
2024-08-29 22:02:20 +0300

    Update Trolley.js

--- src/components/Trolley/Trolley.js
+++ src/components/Trolley/Trolley.js
@@ -11,27 +11,21 @@ const Trolley = () => {
     return (
         <div className={styles['outer']}>
             <div className={styles['trolley']}>
-
                 <header className={styles['trolley-head']}>
                     <h3 className={styles['trolley-head-tittle']}><i className="fa-solid fa-cart-shopping"></i>КОЛИЧКА</h3>
                 </header>
-
                 <div className={styles['items']}>
                     <ul className={styles['items-aded']}>
-
                         <TrolleyItem />
-
                     </ul>
                     <div className={styles['calc-sum']}>
                         <small className={styles['small']}>МЕЖДИННА СУМА: {totalPrice.toFixed(2)}</small>
                         <h3 className={styles['calc-sum-head']}>лв.</h3>
                     </div>
                 </div>
-
                 <footer className={styles['trolley-footer']}>
                     <Link to='/createorder' className={styles['trolley-link-btn']}>ПОРЪЧАЙ</Link>
                 </footer>
-
             </div>
         </div>
     );
