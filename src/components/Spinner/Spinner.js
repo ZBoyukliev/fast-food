39e80fa1e432b5c9e6eafef7abb993b73e38834f
Zarko Boyukliev <122691228+ZBoyukliev@users.noreply.github.com>
2024-08-30 23:34:47 +0300

    Update Spinner.js

--- src/components/Spinner/Spinner.js
+++ src/components/Spinner/Spinner.js
@@ -2,9 +2,7 @@ import styles from './Spinner.module.css';
  
 const Spinner = () =>
     <div className={styles['spinner-container']}>
- 
         <div className={styles['loader']}></div>
     </div>
-    ;
  
-export default Spinner;
+export default Spinner
